0474aa968db9ec011edb30ee36792410e239091e
yamakoshi <takuyageemzuki@gmai.com>
2023-12-22 23:44:32 +0900

    Enable Python testing with pytest and update translation settings - Enable automatic formatting and Python testing with pytest, and disable unittest. - Update comment translation target language setting to Japanese. - Remove unnecessary code comments. - Improve codebase maintainability and testing capabilities.

--- Chrome＿Automation.py
+++ Chrome＿Automation.py
@@ -1,15 +1,21 @@
-import pdb
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 driver = webdriver.Chrome()  # Chromeを自動操作するWebDriverオブジェクトを作成
 driver.get("https://www.google.com")  # Googleのページを開く
 
-search_box = driver.find_element_by_name("q")  # 検索ボックスの要素を取得
-search_box.send_keys("python")  # 検索ボックスに"Selenium"と入力
+# 検索ボックスの要素を取得して"Selenium"と入力し、検索を実行
+search_box = driver.find_element(By.NAME, "q")
+search_box.send_keys("python")  # ここで検索ボックスに"python"と入力
 search_box.send_keys(Keys.RETURN)  # Enterキーを送信して検索を実行
 
-# 結果ページで何か操作をする場合は、ここでコードを追加します。
-
-# driver.quit()  # ブラウザを閉じて終了
-print("Done!")
+# 検索結果が表示されるまで待機
+wait = WebDriverWait(driver, 10)
+first_result = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "h3")))
+# 最初の検索結果をクリック
+first_result.click()
+# その他の操作を行うコードをここに追加
+# 最後にブラウザを閉じる
+# driver.quit()
